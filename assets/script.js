e1a6a89cc2645e0ee79f69044ebca3475d246bbf
Fabulosa <fabiscg79@gmail.com>
2021-11-19 13:16:35 -0500

    created function that set the correct color for the textarea depending upon the time of day

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,26 @@
 var date = moment().format('dddd, MMMM Do, YYYY');
 $("#currentDay").text(date);
+
+var currentTime = moment().hour()
+var timeBlock = $("textarea")
+
+setTimeColor()
+
+function setTimeColor() {
+    timeBlock.each(function() {
+        var currentHour = $(this).attr('id')
+
+        if(currentTime > currentHour) {
+            $(this).addClass('past')
+        }
+        else if (currentTime == currentHour) {
+            $(this).removeClass('past')
+            $(this).addClass('present')
+        }
+        else {
+            $(this).removeClass('past')
+            $(this).removeClass('present')
+            $(this).addClass('future')
+        }
+    })
+}
