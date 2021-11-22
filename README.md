51154879ef6020e7a71478779e121ed7ad89dfb5
Fabulosa <fabiscg79@gmail.com>
2021-11-21 23:50:23 -0500

    finishing the README file and scrip

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# calander
+# Calendar
+
+## It is a Work-Day-Scheduler
+## As a developer I need to be able to plan my day andbe able to have it in my calendar to see my meeting ,classes, homework and another things realted to school.
+
+*I had created this page with the specific hours that I usually use to stydy and do homework*
+
+At the top of the page we have the title and under the title I display the day of the week, month, day and year that updates in regular basis to make sure i am not missing any class
+
+## Also I have the ability to write the task or plan od the day ahead of me and save it in the calendar by clicking in the save button 
+
+## As it is save the calendar also has a dynamic colors to make sure ityou dont missed anything
+
+### 1-Gray: past
+
+### 2-Red:Present(time for the task)
+
+### 3-Green: Future tasks or plans
+
+
--- assets/script.js
+++ assets/script.js
@@ -14,47 +14,43 @@ setTimeColor()
 
 function setTimeColor() {
     timeBlock.each(function () {
-        var currentHour = $(this).attr('id')
+        var currentHour = $(this).attr("id")
 
         if (currentTime > currentHour) {
-            $(this).addClass('past')
+            $(this).addClass("past")
         }
         else if (currentTime == currentHour) {
-            $(this).removeClass('past')
-            $(this).addClass('present')
+            $(this).removeClass("past")
+            $(this).addClass("present")
         }
         else {
-            $(this).removeClass('past')
-            $(this).removeClass('present')
-            $(this).addClass('future')
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
         }
     })
 }
 // created a variable called id for the id(attr) of the save button that we are clicking on
 saveButton.on("click", function () {
-    var task = $(this).siblings("textarea").val;
-    var hour = $(this).parent().attr("id", "");
+    var task = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
 
     localStorage.setItem(hour, task);
 
 
-    // created a variable called task for the siblings(textarea) of the save button that is being clicked on, and what we want from that siblings(textarea) is the val() 
 });
 
-function showTasks() {
 
-    // loop over time blocks
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1])
-
-    });
-
-
-// }
-    // create a for loop to loop through your local storage to get any key that are name anywhere from 9 to 17 hint:(var = 9; i < 18;)
-    // in the for loop create a var that has the value of local storage get item i. 
-    // then display the data to the textarea with the same id as the current key
-// }
+$("#hour9 .task").val(localStorage.getItem("hour9"));
+$("#hour10 .task").val(localStorage.getItem("hour10"));
+$("#hour11 .task").val(localStorage.getItem("hour11"));
+$("#hour12 .task").val(localStorage.getItem("hour12"));
+$("#hour13 .task").val(localStorage.getItem("hour13"));
+$("#hour14 .task").val(localStorage.getItem("hour14"));
+$("#hour15 .task").val(localStorage.getItem("hour15"));
+$("#hour16 .task").val(localStorage.getItem("hour16"));
+$("#hour17 .task").val(localStorage.getItem("hour17"));
 
 
-// showTasks()
+
+showTasks()
